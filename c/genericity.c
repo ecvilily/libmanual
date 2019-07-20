8857d6b8b92cf4f5d759e5cecaf2d44d25bedae9
fastfoxes <38584458+fastfoxes@users.noreply.github.com>
2019-07-20 17:02:03 +0800

    move to c

---
+++ c/genericity.c
@@ -0,0 +1,15 @@
+
+double atof(const char *str)
+int atoi(const char *str)
+long int atol(const char *str)
+
+double fabs(double x)
+int abs(int x)
+long int labs(long int x)
+
+double strtod(const char *str, char **endptr)
+long int strtol(const char *str, char **endptr, int base)
+unsigned long int strtoul(const char *str, char **endptr, int base)
+
+div_t div(int numer, int denom)
+ldiv_t ldiv(long int numer, long int denom)
---
+++ c/math.c
@@ -0,0 +1,7 @@
+double log(double x) double log10(double x)
+double pow(double x, double y)
+double sqrt(double x)
+double ceil(double x)
+
+double floor(double x)
+double fmod(double x, double y)
